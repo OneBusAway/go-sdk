8188c7225b359800379eb8591c09fca2c4215d29
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-09-13 00:20:23 +0300

    feat: Go sdk examples

--- examples/stops-for-location/main.go
+++ examples/stops-for-location/main.go
@@ -6,8 +6,8 @@ import (
 	"log"
 	"strings"
 
-	onebusaway "github.com/stainless-sdks/open-transit-go"
-	"github.com/stainless-sdks/open-transit-go/option"
+	onebusaway "github.com/OneBusAway/go-sdk"
+	"github.com/OneBusAway/go-sdk/option"
 )
 
 func main() {
